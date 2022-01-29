d057a2897cc30cbcb6610d070fbed79654ef6dea
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-01-29 18:54:57 +0100

    switched show patients to table view

--- frontend/views.py
+++ frontend/views.py
@@ -3,16 +3,32 @@ from django.http import HttpResponse
 
 patients = [
     {
-        'first_name': 'Max',
-        'last_name': 'Powers',
+        'id': '12345',
+        'first_name': 'Legolas',
+        'last_name': 'Grunblatt',
         'main_diagnosis': 'Awesomeritis',
         'date_of_birth': '01.01.1990'
     },
     {
+        'id': '67899',
         'first_name': 'Gandalf',
         'last_name': 'Gray',
         'main_diagnosis': 'Whiteness',
         'date_of_birth': '01.01.1800'
+    },
+    {
+        'id': '67289',
+        'first_name': 'Gimli',
+        'last_name': 'Gloinsson',
+        'main_diagnosis': 'Stubbornness',
+        'date_of_birth': '01.01.1970'
+    },
+    {
+        'id': '25942',
+        'first_name': 'Aragorn',
+        'last_name': 'Streicher',
+        'main_diagnosis': 'Kingliness',
+        'date_of_birth': '01.01.1975'
     }
 ]
 
