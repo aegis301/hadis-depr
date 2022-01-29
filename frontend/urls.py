a87b70a4c94456cc3a34000c43e94483f831dc81
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-01-29 13:32:34 +0100

    setup basic pages and implement bootstrap

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='hadis-home')
+    path('', views.home, name='hadis-home'),
+    path('forms/', views.forms, name='show-forms'),
+    path('show_patients/', views.show_page, name='show_page')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+patients = [
+    {
+        'first_name': 'Max',
+        'last_name': 'Powers',
+        'main_diagnosis': 'Awesomeritis',
+        'date_of_birth': '01.01.1990'
+    },
+    {
+        'first_name': 'Gandalf',
+        'last_name': 'Gray',
+        'main_diagnosis': 'Whiteness',
+        'date_of_birth': '01.01.1800'
+    }
+]
+
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'frontend/home.html')
+
+
+def forms(request):
+    return render(request, 'frontend/forms.html')
+
+
+def show_page(request):
+    context = {
+        'patients': patients,
+        'title': 'Show Patients'
+    }
+    return render(request, 'frontend/show_page.html', context)
