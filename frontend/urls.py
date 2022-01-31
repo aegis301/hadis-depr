debd3874c45eafee13a5ffb3751936cce3b95c55
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-01-31 21:50:34 +0100

    add hardcoded basic forms

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='hadis-home'),
-    path('forms/', views.forms, name='show-forms'),
-    path('show_patients/', views.show_page, name='show_page')
+    path('forms/', views.forms, name='forms-show'),
+    path('show_patients/', views.show_page, name='patients-show')
 ]
