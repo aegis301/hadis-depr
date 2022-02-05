9dd5114b5460d08b56757bb2310556b3a1c01ff5
Christian Porschen <c_pors01@uni-muenster.de>
2022-02-05 18:23:33 +0100

    add postgres support

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hadis',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
 }
 
