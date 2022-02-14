ef1c7c19e865b2c90d1b8ed86a43938fd465064b
Christian Porschen <c_pors01@uni-muenster.de>
2022-02-14 21:03:54 +0100

    began working on login feature

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,13 @@
 from unicodedata import name
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='hadis-home'),
     path('forms/', views.forms, name='forms-show'),
     path('show_patients/', views.show_page, name='patients-show'),
     path('user_registration/', views.register_user, name='user-registration'),
-    path('user_login', views.login_user, name="user-login")
+    path('user_login', auth_views.LoginView.as_view(template_name='frontend/login.html'), name="user-login"),
+    path('user_logout', auth_views.LogoutView.as_view(template_name='frontend/logout.html'), name="user-logout")
 ]
