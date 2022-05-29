befd4cd7c3f49bb60fafd3281aade6d6a0bdb402
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-05-29 19:14:43 +0200

    jsx support

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     filename: 'index-bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './static'),  // path to our Django static directory
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    ]
+  }
 };
+
