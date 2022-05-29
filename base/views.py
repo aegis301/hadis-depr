2d84e74c5fe0f1f46b63a41ced2a3a0ba0cee48b
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-05-29 21:42:51 +0200

    add link to issue

--- base/views.py
+++ base/views.py
@@ -11,5 +11,5 @@ class JavascriptView(TemplateView):
     
     def get_context_data(self, **kwargs):
         return {
-            
+            ## TODO: #9 Go on from here with https://www.saaspegasus.com/guides/modern-javascript-for-django-developers/integrating-django-react/#the-demo-of-what-well-be-building
         }
