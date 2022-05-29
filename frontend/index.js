56409cfda2e87280f21c47c12aff4fc166cf22ea
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-05-29 19:13:07 +0200

    use npm external packages

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
+
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  element.innerHTML =  _.join(['Hello', 'lodash'], ' ');
+  return element;
+}
+document.body.appendChild(component());
